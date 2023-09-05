aad973fac91f3d035d18b35f86bfa424941d32ae
sidorenko_ev <evgeny.sidorenko@gmail.com>
2023-09-05 15:38:45 +0300

    some small changes

--- Tunnel Client/minecraft/scripts/prepare-solution.sh
+++ Tunnel Client/minecraft/scripts/prepare-solution.sh
@@ -2,7 +2,6 @@
 set -e
 
 solution_path=$1
-#solution_name=$2
 
 # cd to one directory up of current script
 REAL_PATH=$(realpath "$0")
@@ -13,9 +12,6 @@ cd ..
 echo "Install dependencies for client & sever module"
 yarn dependencies-scripts
 yarn build:all
-
-echo "Make logs dir"
-mkdir "$solution_path"/logs
 
 echo "Copy files"
 cp -R dist node_modules package.json yarn.lock server client "$solution_path"
