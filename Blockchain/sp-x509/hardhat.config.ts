a031db6385e1d5945e50af88e8f90bd0a9a8dbe8
Marat Idrisov <idrisovm15@gmail.com>
2023-12-08 00:22:01 +0000

    x509 config update

--- Blockchain/sp-x509/hardhat.config.ts
+++ Blockchain/sp-x509/hardhat.config.ts
@@ -51,12 +51,12 @@ export default {
         },
         local: {
             url: 'http://localhost:8545',
-            account: config.deployerPrivateKey,
+            account: config.mumbaiDeployerPrivateKey,
         },
         mumbai: {
             chainId: 80001,
             url: config.mumbaiUrl,
-            accounts: [config.deployerPrivateKey],
+            accounts: [config.localhostDeployerPrivateKey],
         },
     },
     etherscan: {
