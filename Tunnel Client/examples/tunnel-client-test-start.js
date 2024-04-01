da295e5d4872c4e9126ba14d1ea1068f9e3ea95a
Vladyslav Marchuk <134324829+marchuk-vlad@users.noreply.github.com>
2024-04-01 20:06:06 +0300

    Added tunnel client test start script (#41)
    * Added tunnel client test start script
    
    * Handle system signals before server check
    
    * Snake kase to kebab case

---
+++ Tunnel Client/examples/tunnel-client-test-start.js
@@ -0,0 +1,142 @@
+const wt = require('worker_threads');
+const { exec } = require('child_process');
+const util = require('util');
+const fs = require('fs').promises;
+const https = require('https');
+
+const execPromise = util.promisify(exec);
+
+const serverJsPath = process.argv[2];
+
+if (!serverJsPath) {
+  throw 'Path to server.js is not set as argument';
+}
+
+const checkWithInterval = async (handler, intervalTime, maxChecks) => {
+  console.log(`Checking with interval ${intervalTime}`);
+  let interval = null;
+  let checkedTimes = 0;
+  try {
+    return await new Promise((resolve, reject) => {
+      const intervalFn = async () => {
+        try {
+          const result = await handler();
+
+          resolve(result);
+        } catch (err) {
+          console.error(err.message, `Retrying...`);
+          checkedTimes += 1;
+          if (checkedTimes >= maxChecks) {
+            console.error(err.message, `checkWithInterval: MaxCheck count reached! Abort...`);
+
+            reject(err);
+          }
+        }
+      };
+
+      interval = setInterval(intervalFn, intervalTime);
+    });
+  } finally {
+    clearInterval(interval);
+  }
+};
+
+const privateKeyFile = `private.pem`;
+const certificateFile = `certificate.crt`;
+const opensslCommand = `openssl req -newkey rsa:2048 -nodes -keyout ${privateKeyFile} -x509 -days 365 -out ${certificateFile} -subj "/CN=localhost"`;
+
+const port = 9090;
+
+async function removeLocalCerts() {
+  await fs.unlink(privateKeyFile).catch(() => {} /* suppress */);
+  await fs.unlink(certificateFile).catch(() => {} /* suppress */);
+}
+
+async function run() {
+  const checkInterval = 10000; //10 sec
+  try {
+    await execPromise(opensslCommand);
+    const tlsKey = (await fs.readFile(privateKeyFile)).toString();
+    const tlsCert = (await fs.readFile(certificateFile)).toString();
+    console.log('Starting worker');
+
+    const worker = new wt.Worker(serverJsPath, {
+      env: {
+        ...process.env,
+        HTTPS_PORT: port,
+        TLS_CERT: tlsCert,
+        TLS_KEY: tlsKey,
+      },
+    });
+
+    worker.once('error', (error) => {
+      console.error(error.message, 'Worker error was occurred');
+    });
+
+    worker.once('exit', (exitCode) => {
+      console.log('Worker was exist');
+
+      if (exitCode !== 0) {
+        throw new Error(`Cannot start server. Exit code: ${exitCode}`);
+      }
+    });
+
+    ['SIGINT', 'SIGTERM'].forEach((signal) => {
+      process.on(signal, () => {
+        worker.postMessage(signal);
+      });
+    });
+
+    const checkFn = () => {
+      return new Promise((resolve, reject) => {
+        const requestOptions = {
+          port,
+          method: 'GET',
+          host: 'localhost',
+          ca: tlsCert,
+        };
+
+        const req = https.request(requestOptions, (res) => {
+          const certificate = res.socket.getPeerCertificate(false);
+          if (!certificate) {
+            reject(new Error(`There isn't certificate for internal server`));
+          }
+          console.log('Certificate is checked');
+          resolve();
+        });
+        req.on('error', (error) => {
+          reject(error);
+        });
+        req.on('timeout', () => {
+          reject(new Error('timeout error'));
+        });
+        req.end();
+      });
+    };
+
+    const maxChecks = 1 + Math.floor(30000 / checkInterval);
+
+    const terminationHandler = (signal) => {
+      console.log(`${signal} received`);
+      worker.terminate();
+      removeLocalCerts().then(() => process.exit(0));
+    };
+
+    process
+      .on('SIGINT', () => terminationHandler('SIGINT'))
+      .on('SIGTERM', () => terminationHandler('SIGTERM'))
+      .on('SIGABRT', () => terminationHandler('SIGABRT'));
+
+    await checkWithInterval(checkFn, checkInterval, maxChecks);
+
+    console.log('Server started');
+  } catch (error) {
+    console.error(error.message, 'Fail to start a server');
+
+    throw error;
+  } finally {
+    await removeLocalCerts();
+  }
+}
+
+run().catch(console.err);
