32677a542481aec0d5d663f3f907f979a3cc1767
root <root@465698.xorek.cloud>
2025-06-27 18:29:30 +0300

    fix: correct config.json error, fix Dockerfile env issue, add submodule

--- Dia TTS Server/src/config.ts
+++ Dia TTS Server/src/config.ts
@@ -1,46 +1,24 @@
-import { TunnelClient, TunnelClientOptions } from '@super-protocol/tunnels-lib';
-import {
-  readConfiguration,
-  getDomainConfigs,
-  TunnelsConfiguration,
-  exitOnUnhandledRejection,
-  exitOnUncaughtException,
-  exitOnSignals,
-} from '@super-protocol/solution-utils';
-import { rootLogger } from './logger';
-import { config } from './config';
+import dotenv from 'dotenv';
+import path from 'path';
+import packageJson from '../package.json';
+import { getEnvValueOrFail } from './env-utils';
 
-exitOnUnhandledRejection(rootLogger);
-exitOnUncaughtException(rootLogger);
-exitOnSignals(rootLogger);
+dotenv.config();
 
-const run = async (): Promise<void> => {
-  const logger = rootLogger.child({ method: run.name });
-  const configuration = await readConfiguration(config.configurationPath);
-  const tunnelsConfiguration = configuration?.solution?.tunnels as TunnelsConfiguration | undefined;
-
-  const domainConfigs = await getDomainConfigs({
-    tunnels: tunnelsConfiguration,
-    blockchainUrl: config.blockchainUrl,
-    contractAddress: config.blockchainContractAddress,
-    logger,
-  });
-
-  logger.debug(
-    { domains: domainConfigs.map((config) => config.site.domain) },
-    'Found tunnel client domain configs',
-  );
-
-  const options: TunnelClientOptions = {
-    logger: rootLogger,
-    applicationPort: config.clientServerPort,
-    localServerStartTimeout: config.localServerStartTimeoutMs,
-  };
-  const tunnelClient = new TunnelClient(config.serverFilePath, domainConfigs, options);
-  await tunnelClient.start();
+export const config = {
+  appName: packageJson.name,
+  appVersion: packageJson.version,
+  blockchainUrl: getEnvValueOrFail('BLOCKCHAIN_URL'),
+  blockchainContractAddress: getEnvValueOrFail('BLOCKCHAIN_CONTRACT_ADDRESS'),
+  logLevel: (process.env.LOG_LEVEL as string) || 'trace',
+  inputDataFolder: (process.env.INPUT_DATA_FOLDER as string) || '/sp/inputs',
+  secretsDataFolder: (process.env.SECRETS_DATA_FOLDER as string) || '/sp/secrets',
+  certFileName: (process.env.CERT_FILE_NAME as string) || 'certificate.crt',
+  certPrivateKeyFileName: (process.env.CERT_PRIVATE_KEY_FILE_NAME as string) || 'private.pem',
+  configFileName: 'tunnel-client-config.json',
+  configSearchFolderDepth: 1 as number,
+  clientServerPort: 9000,
+  serverFilePath: path.join(__dirname, './server.js'),
+  configurationPath: process.env.CONFIGURATION_PATH || '/sp/configurations/configuration.json',
+  localServerStartTimeoutMs: Number.parseInt(process.env.LOCAL_SERVER_START_TIMEOUT_MS || '900000'), // 15 min
 };
-
-run().catch((err) => {
-  rootLogger.fatal({ err }, `Failed to start application`);
-  process.exit(1);
-});
