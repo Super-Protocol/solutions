52a7ff4ac4492c8fb6e56301fd77df39dff548f2
Vladyslav Marchuk <134324829+marchuk-vlad@users.noreply.github.com>
2025-08-27 13:06:46 +0300

    SP-6855: jupyter solution (#126)
    * feat(jupyter): Add jupyter notebook solution
    
    * temp: show resource
    
    * feat(build-solution): add Jupyter build
    
    * build(configuration): Add Jupyter
    
    * fix(jupyter-config): Added models and datasets
    
    * Revert "temp: show resource"
    
    This reverts commit d450c876c8ebc901121443f5a6b3a306d8ed8ab1.
    
    * fix(jupyter): comments fixes
    
    * fix(ipynb): improve command parameters structure and logging

---
+++ Jupyter/src/ipynb.ts
@@ -0,0 +1,70 @@
+import fs from 'fs';
+import path from 'path';
+import { spawn } from 'child_process';
+import { FindFileResult } from './types';
+import { rootLogger } from './logger';
+import { config } from './config';
+
+export const findIpynbFile = async (
+  folderPath: string,
+  filename?: string,
+): Promise<FindFileResult | undefined> => {
+  const logger = rootLogger.child({ module: 'findIpynbFile' });
+
+  try {
+    const filesOrDirectories = await fs.promises.readdir(folderPath, { withFileTypes: true });
+    for (const fileOrDir of filesOrDirectories) {
+      if (fileOrDir.isDirectory()) {
+        const subFolderPath = `${folderPath}/${fileOrDir.name}`;
+        const foundFile = await findIpynbFile(subFolderPath, filename);
+        if (foundFile) {
+          return foundFile;
+        }
+      } else if (fileOrDir.name.endsWith('.ipynb')) {
+        if (!filename || fileOrDir.name === filename) {
+          return { folder: folderPath, filename: fileOrDir.name };
+        }
+      }
+    }
+  } catch (error) {
+    logger.error({ err: error }, 'Error while searching for ipynb file');
+    return;
+  }
+};
+
+export const runIpynbFile = async (filePath: string): Promise<void> => {
+  const logger = rootLogger.child({ module: 'runIpymbFile' });
+  const filename = path.basename(filePath);
+
+  try {
+    // Run nbconvert in-place on the notebook file. Do not add shell quotes around filePath.
+    const commandParams = [
+      `nbconvert`,
+      `--execute`,
+      `--to`,
+      `notebook`,
+      `--inplace`,
+      filePath,
+      `--stdout`,
+    ];
+
+    const logPath = path.join(config.outputDataFolder, `${filename}.log`);
+    const logStream = fs.createWriteStream(logPath);
+
+    logger.info({ commandParams }, `Executing jupyter with params`);
+    const commandProcess = spawn('jupyter', commandParams, { stdio: 'pipe' });
+    commandProcess.stdout.pipe(logStream);
+    commandProcess.stderr.pipe(logStream);
+
+    await new Promise((resolve, reject) => {
+      commandProcess.on('close', (code) => {
+        if (code === 0) resolve(void 0);
+        else reject(new Error(`Process exited with code ${code}`));
+      });
+      commandProcess.on('error', reject);
+    });
+  } catch (error) {
+    logger.error({ err: error }, `Failed to run ipynb file: ${filePath}`);
+    throw error;
+  }
+};
